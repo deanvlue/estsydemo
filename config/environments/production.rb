46eb0c5654f9ed411f0cdc84b5525d89ff43ed44
Carlos Muñoz <munoz.josecarlos@gmail.com>
2014-07-09 10:03:57 -0500

    changes on the font and color

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for devise gem. remember to change localhost:3000 to production URL
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
